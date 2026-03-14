0575638f4766ab6b5379883c4b347b5611c354f1
sprout-hatanaka <sprout.hatanaka@gmail.com>
2026-03-14 11:32:13 +0900

    アイテム画像の変更を実装

--- Desktop/simple-closet/.next/dev/static/chunks/[project]_src_app_icon_svg_mjs_098435f7._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/77dae_next_dist_3127f467._.js"
-],
-    source: "dynamic"
-});
