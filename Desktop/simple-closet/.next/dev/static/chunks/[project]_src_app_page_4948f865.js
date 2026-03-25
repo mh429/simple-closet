809f9593a46be8959aedf336eb3e9aa773356871
sprout-hatanaka <sprout.hatanaka@gmail.com>
2026-03-25 14:30:06 +0900

    メッセージ修正

--- Desktop/simple-closet/.next/dev/static/chunks/[project]_src_app_page_4948f865.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/77dae_next_dist_d8be5446._.js",
-  "static/chunks/[project]_src_components_Weather_cb093434.js"
-],
-    source: "dynamic"
-});
